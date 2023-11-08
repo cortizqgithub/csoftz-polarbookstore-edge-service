2f8d2abf8678f40b2e6a48b1aa7410eac9c0de17
Carlos Adolfo Ortiz Quirós <carlosaortiz@yahoo.com>
2023-11-07 22:29:33 -0500

    Add initial files

---
+++ src/test/java/com/polarbookshop/edgeservice/ApplicationTests.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   APPLICATIONTESTS.JAVA                                       */
+/* Copyright (c), 2023 CSoftZ                                                 */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ Nov.04/2023  COQ  File created.
+ -----------------------------------------------------------------------------*/
+package com.polarbookshop.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit tests for Application class.
+ *
+ * @author COQ - Carlos Adolfo Ortiz Q.
+ */
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
